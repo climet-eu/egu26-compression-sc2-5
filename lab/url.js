624df378b36ab30789bd2fef6c5f27686a79edfd
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-04-23 15:41:00 +0300

    make pre-mounting code more fault-tolerant

--- lab/url.js
+++ lab/url.js
@@ -34,17 +34,33 @@ console.log(
         EARTHKIT_DATA_CACHE_POLICY: "off",
         EARTHKIT_REGRID_CACHE_POLICY: "off",
         CLIMET_LAB_BOOTSTRAP_CODE: `\
+import warnings
 from pathlib import Path
 
 import pyodide_fs_mount_http
 
-data = Path("data")
 
-for folder, files in ${JSON.stringify(all_data_paths)}.items():
-    pyodide_fs_mount_http.mount_http_files(data / folder, {
-        name: f"https://media.githubusercontent.com/media/${repo.user}/${repo.name}/refs/heads/${repo.branch}/data/{name}"
-        for name in files
-    })
+def mount_data_files(*args, **kwargs):
+    if mount_data_files.mounted:
+        return
+    mount_data_files.mounted = True
+
+    data = Path("data")
+
+    for folder, files in ${JSON.stringify(all_data_paths)}.items():
+        if data.joinpath(folder).exists():
+            continue
+
+        try:
+            pyodide_fs_mount_http.mount_http_files(data / folder, {
+                name: f"https://media.githubusercontent.com/media/${repo.user}/${repo.name}/refs/heads/${repo.branch}/data/{name}"
+                for name in files
+            })
+        except Exception as err:
+            warnings.warn(f"Failed to mount data files in {data / folder}: {err}")
+mount_data_files.mounted = False
+
+ip.events.register("pre_execute", mount_data_files)
 `,
       }),
     ),

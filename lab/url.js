2ca72a63b708570c62d03afeb7b9a1b12dbf42d2
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-04-23 12:37:58 +0300

    use simpler lab link

--- lab/url.js
+++ lab/url.js
@@ -1,22 +1,35 @@
+const fs = require("fs");
+
+const lab = {
+  version: "main",
+};
+
+const repo = {
+  user: "climet-eu",
+  name: "egu26-compression-sc2-5",
+  branch: "main",
+};
+
+const all_data_names = fs.readdirSync("data");
+
 console.log(
-  "https://lab.climet.eu/main/github/climet-eu/egu26-compression-sc2.5/HEAD?pyodideKernelEnv=" +
+  `https://lab.climet.eu/${lab.version}/lab/index.html?fromURL=` +
+    `https://raw.githubusercontent.com/${repo.user}/${repo.name}/refs/heads/${repo.branch}/compression.ipynb` +
+    "?pyodideKernelEnv=" +
     encodeURIComponent(
       JSON.stringify({
         EARTHKIT_DATA_CACHE_POLICY: "off",
         EARTHKIT_REGRID_CACHE_POLICY: "off",
         CLIMET_LAB_BOOTSTRAP_CODE: `\
-import shutil
 from pathlib import Path
 
 import pyodide_fs_mount_http
 
 data = Path("data")
-all_data_names = [p.name for p in data.iterdir()]
-shutil.rmtree(data)
 
 pyodide_fs_mount_http.mount_http_files(data, {
-    name: f"https://media.githubusercontent.com/media/climet-eu/egu26-compression-sc2.5/refs/heads/main/data/{name}"
-    for name in all_data_names
+    name: f"https://media.githubusercontent.com/media/${repo.user}/${repo.name}/refs/heads/${repo.branch}/data/{name}"
+    for name in ${JSON.stringify(all_data_names)}
 })
 `,
       }),
